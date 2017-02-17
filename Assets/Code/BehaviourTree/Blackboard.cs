3f944f27aac95f965e9699d26c2d40d1e85d8cec
ZeeQyu <mcmastasadventures@gmail.com>
2017-02-17 09:34:44 +0100

    Add inital blackboard

---
+++ Assets/Code/BehaviourTree/Blackboard.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BehaviourTree
+{
+    class Blackboard
+    {
+        Dictionary<uint, object> _members;
+        Blackboard()
+        {
+            _members = new Dictionary<uint, object>();
+        }
+
+        // Accessors
+        // As object
+        object GetData(uint key)
+        {
+            return _members[key];
+        }
+        object GetData(string keyString)
+        {
+            uint key = FNV1Hash.Calculate(keyString);
+            return _members[key];
+        }
+        // As specific type
+        T GetAs<T>(uint key)
+        {
+            Debug.Assert(_members[key].GetType() == typeof(T), string.Format("Tried to get a {} as a bool", _members[key].GetType()));
+            return (T)_members[key];
+        }
+        T GetAs<T>(string keyString)
+        {
+            uint key = FNV1Hash.Calculate(keyString);
+            return GetAs<T>(key);
+        }
+        // Mutators
+        void Set(uint key, object value)
+        {
+            _members[key] = value;
+        }
+        void Set(string keyString, object value)
+        {
+            uint key = FNV1Hash.Calculate(keyString);
+            Set(key, value);
+        }
+        void Remove(uint key)
+        {
+            _members.Remove(key);
+        }
+        void Remove(string keyString)
+        {
+            uint key = FNV1Hash.Calculate(keyString);
+            Remove(key);
+        }
+    }
+}
---
+++ Assets/Code/BehaviourTree/Common.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BehaviourTree
+{
+    class FNV1Hash
+    {
+        public static uint Calculate(string inHashString)
+        {
+            const uint fnvPrime = 0x01000193;
+            const uint offsetBasis = 0x811C9DC5;
+            uint hash = offsetBasis;
+
+            foreach (char character in inHashString)
+            {
+                hash *= fnvPrime;
+                hash ^= character;
+            }
+            return hash;
+        }
+    }
+}
